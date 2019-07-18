1f011015b6f70f3be606227f965852c6812f7068
changle <changle@changledeMacBook-Pro.local>
2019-07-19 00:23:49 +0800

    add wx

--- CrawlService/src/main/java/com/module/taobao/PyMain.java
+++ CrawlService/src/main/java/com/module/taobao/PyMain.java
@@ -20,11 +20,108 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class PyMain {
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws Exception {
 		//tb_getdetail();
-		
 		//tb_homepage_recommend();
+		tb_live();
 	}
+	
+	private static void tb_live() throws Exception{
+		//		request:Request{ url=
+		//?data=	//method=GET, 
+		//appKey=21646297, 
+		//authCode=null, 
+		
+		//headers:{data={"PARCELABLE_WRITE_RETURN_VALUE":"1","s":"20","channelId":"0","CONTENTS_FILE_DESCRIPTOR":"1","n":"10","CREATOR":"{}","queryAd":"1","moduleIndex":"12","haveOnlook":"false","version":"10"}}
+		//			nBacktrace:njava.lang.Exception
+		
+		
+		
+		
+		
+		String data= "%7B%22PARCELABLE_WRITE_RETURN_VALUE%22%3A%221%22%2C%22s%22%3A%2220%22%2C%22channelId%22%3A%220%22%2C%22CONTENTS_FILE_DESCRIPTOR%22%3A%221%22%2C%22n%22%3A%2210%22%2C%22CREATOR%22%3A%22%7B%7D%22%2C%22queryAd%22%3A%221%22%2C%22moduleIndex%22%3A%2212%22%2C%22haveOnlook%22%3A%22false%22%2C%22version%22%3A%2210%22%7D";		
+		String strData = new String(URLDecoder.decode(data));
+		HashMap<String, String> headers = new HashMap<String, String>();
+		headers.put("content-type","application/x-www-form-urlencoded");
+		headers.put("charset", "UTF-8");
+		headers.put("x-umt", "TctLrt5LOv477jVr1Lfpe1SkhcRTb4TU");
+		headers.put("x-region-channel","CN");
+		headers.put("x-app-conf-v", "19");
+		headers.put("x-features","27"); 
+		headers.put("x-nettype","WIFI"); 
+		headers.put("x-utdid","ADqYe0dIN3UDAOMIFobd3OTJ"); 
+		headers.put("user-agent","MTOPSDK%2F3.1.1.7+%28Android%3B4.4.4%3BLGE%3BAOSP+on+HammerHead%29");
+		headers.put("x-nq", "WIFI");
+		headers.put("x-pv", "6.1");
+		headers.put("x-mini-wua","HHnB_Mq8mo9OW9cLG7UQ7MwO6UkcM3Ke9zpVnTn8ovWVW0CwaDO2GkQkk5rDi2RsyxRbEo1QfVnb0UcJ0WGyerboH%2Fz7Vf5tuuWixYCW9Cx9lL5b8FwmPrtilyha3LFuAToxr"); 
+		headers.put("x-location", "114.418888%2C30.466112");
+		headers.put("x-devid","AiKqef86Ry_ZEna6MEEvoW2BU6NZnN83riONd8NKdV6d"); 
+		headers.put("x-c-traceid","ADqYe0dIN3UDAOMIFobd3OTJ15630875178200034110836"); 
+		headers.put("x-appkey","21646297"); 
+		headers.put("x-m-biz-live-bizcode","TAOBAO"); 
+		headers.put("x-app-ver","8.6.10"); 
+		headers.put("x-ttid", "1551686692688%40taobao_android_8.6.10");
+		headers.put("x-page-url","http%3A%2F%2Fh5.m.taobao.com%2Ftaolive%2Fmain.html"); 
+		headers.put("x-page-name","com.taobao.taolivehome.TaoLiveHomepageActivity"); 
+		headers.put("f-refer","mtop"); 
+		headers.put("c-launch-info","0,0,1563087517814,1563083666000"); 
+		headers.put("body","null"); 
+		headers.put("seqNo","MTOP34"); 
+		headers.put("connectTimeoutMills","10000"); 
+		headers.put("readTimeoutMills","15000"); 
+		headers.put("retryTimes", "1"); 
+		headers.put("env","0"); 
+		headers.put("reqContext","null"); 
+		headers.put("api","mtop.mediaplatform.live.videolist");
+		headers.put("x-t", ""+System.currentTimeMillis()/1000);
+		String sign = calSign("mtop.mediaplatform.live.videolist",strData, headers);
+		headers.put("x-sign", sign);
+
+		//headers.put("x-sign", "ab20380090ce827cfff3459c4f34eb058ff7c697224c262120");
+		
+		
+		
+//		headers.put("x-appkey", "21646297");
+//		headers.put("x-region-channel", "CN");
+//		headers.put("x-mini-wua",
+//				"HHnB_k3SbtdVkHZF91m6SMVlApefpZBJxEl7APRZMFLJZl3nf4n15t7VYK8Du9eMK2Enbotc6CAcVmEZE7RHUn52S0DPHY%2BZzDIrr9kz51%2BU89EE3JywgAUIy7gZ6ewzKlWN7");
+//		headers.put("x-nq", "WIFI");
+//		headers.put("x-c-traceid", "XLNRKmSjZQ0DAKPRVDYM%2FTUU1556533396546014514671");
+//		headers.put("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+//		headers.put("x-app-conf-v", "19");
+//		headers.put("x-features", "27");
+//		headers.put("c-launch-info", "3,0,1556533396545,1556530520000");
+//		headers.put("x-page-name", "com.taobao.android.detail.wrapper.activity.DetailActivity");
+//		headers.put("x-app-ver", "8.6.10");
+//		headers.put("x-t", "1556533396");
+//		headers.put("x-pv", "6.1");
+//		headers.put("x-location", "113.939778%2C22.540102");
+//		headers.put("user-agent", "MTOPSDK%2F3.1.1.7+%28Android%3B6.0%3BHuawei%3BNexus+6P%29");
+//		headers.put("f-refer", "mtop");
+//		headers.put("x-ttid", "1551686692688%40taobao_android_8.6.10");
+//		headers.put("x-nettype", "WIFI");
+//		headers.put("cache-control", "no-cache");
+//		headers.put("x-utdid", "XLNRKmSjZQ0DAKPRVDYM%2FTUU");
+//		headers.put("x-umt", "qCtLWLxLOt%2Bh5TVqZ9rCsntxLqmXyLwx");
+//		headers.put("x-devid", "AkL9reMiyzxVGNsaZS4M8b71xjJdIo9FXw31mH2LHtke");
+//		headers.put("x-page-url", "http%3A%2F%2Fitem.taobao.com%2Fitem.htm}");
+//		
+//		long t= System.currentTimeMillis();
+		
+		
+
+		String url = "https://guide-acs.m.taobao.com/gw/mtop.mediaplatform.live.videolist/5.0/?data="
+			+ strData;
+		OkHttpClient okHttpClient = new OkHttpClient();
+		final Request request = new Request.Builder().url(url)
+				.headers(Headers.of(headers))
+				.get().build();
+		Call call = okHttpClient.newCall(request);
+		Response response = call.execute();
+		System.out.println(response.body().string());
+		
+	}
+	
 	private static void tb_getdetail() throws IOException {
 		String data= "%7B%22detail_v%22%3A%223.3.2%22%2C%22exParams%22%3A%22%7B%5C%22ad_type%5C%22%3A%5C%221.0%5C%22%2C%5C%22appReqFrom%5C%22%3A%5C%22detail%5C%22%2C%5C%22container_type%5C%22%3A%5C%22xdetail%5C%22%2C%5C%22countryCode%5C%22%3A%5C%22CN%5C%22%2C%5C%22device%5C%22%3A%5C%2280%5C%22%2C%5C%22dinamic_v3%5C%22%3A%5C%22true%5C%22%2C%5C%22id%5C%22%3A%5C%224569030103%5C%22%2C%5C%22latitude%5C%22%3A%5C%2222.540046%5C%22%2C%5C%22longitude%5C%22%3A%5C%22113.940264%5C%22%2C%5C%22osVersion%5C%22%3A%5C%2223%5C%22%2C%5C%22phoneType%5C%22%3A%5C%22Nexus+6P%5C%22%2C%5C%22pvid%5C%22%3A%5C%22b32d3077-dca2-4b7d-b1ea-1d21a4b1a11b%5C%22%2C%5C%22scm%5C%22%3A%5C%221007.18849.126744.754%5C%22%2C%5C%22soVersion%5C%22%3A%5C%222.0%5C%22%2C%5C%22spm%5C%22%3A%5C%22a2141.1.guessitemtab_2.9%5C%22%2C%5C%22spm-cnt%5C%22%3A%5C%22a2141.7631564%5C%22%2C%5C%22ultron2%5C%22%3A%5C%22true%5C%22%2C%5C%22utdid%5C%22%3A%5C%22XLNRKmSjZQ0DAKPRVDYM%2FTUU%5C%22%2C%5C%22utparam%5C%22%3A%5C%22%7B%5C%5C%5C%22guessModelVersion%5C%5C%5C%22%3A%5C%5C%5C%22%5C%5C%5C%22%2C%5C%5C%5C%22card_subtype%5C%5C%5C%22%3A%5C%5C%5C%22-%5C%5C%5C%22%2C%5C%5C%5C%22x_object_type%5C%5C%5C%22%3A%5C%5C%5C%22item%5C%5C%5C%22%2C%5C%5C%5C%22matrix_score%5C%5C%5C%22%3A0.0%2C%5C%5C%5C%22miniapp_score%5C%5C%5C%22%3A0.0%2C%5C%5C%5C%22hybrid_score%5C%5C%5C%22%3A0.0%2C%5C%5C%5C%22card_type%5C%5C%5C%22%3A%5C%5C%5C%22-%5C%5C%5C%22%2C%5C%5C%5C%22tpp_buckets%5C%5C%5C%22%3A%5C%5C%5C%228849%230%23126744%238_8849%23191%23754%23394_14189%230%23130950%232_14189%23328%231275%23691%5C%5C%5C%22%2C%5C%5C%5C%22miniapphc_score%5C%5C%5C%22%3A0.0%2C%5C%5C%5C%22pvid%5C%5C%5C%22%3A%5C%5C%5C%22b32d3077-dca2-4b7d-b1ea-1d21a4b1a11b%5C%5C%5C%22%2C%5C%5C%5C%22x_item_ids%5C%5C%5C%22%3A4569030103%2C%5C%5C%5C%22auction_score%5C%5C%5C%22%3A0.0%2C%5C%5C%5C%22x_sytab%5C%5C%5C%22%3A%5C%5C%5C%221002%5C%5C%5C%22%2C%5C%5C%5C%22x_object_id%5C%5C%5C%22%3A4569030103%7D%5C%22%7D%22%2C%22itemNumId%22%3A%224569030103%22%7D";		
 		String strData = new String(URLDecoder.decode(data));
@@ -59,7 +156,7 @@ public class PyMain {
 		headers.put("x-sign", sign);
 
 		String url = "https://trade-acs.m.taobao.com/gw/mtop.taobao.detail.getdetail/6.0/?data="
-				+ data;
+				+ strData;
 		OkHttpClient okHttpClient = new OkHttpClient();
 		final Request request = new Request.Builder().url(url)
 				.headers(Headers.of(headers))
@@ -117,6 +214,7 @@ public class PyMain {
 		System.out.println("" + response.body().string());
 	}
 
+	
 	private static String calSign(String api, String strData, HashMap<String, String> headers) {
 		HashMap<String, String> haspMap = new HashMap<String, String>();
 		String[] locations = URLDecoder.decode(headers.get("x-location")).split(",");
@@ -131,13 +229,13 @@ public class PyMain {
 		haspMap.put("lng", lng);
 		haspMap.put("lat", lat);
 		haspMap.put("v", "1.0");
-		haspMap.put("sid", headers.get("x-sid"));
+		//haspMap.put("sid", headers.get("x-sid"));
 		haspMap.put("t", headers.get("x-t"));
-		haspMap.put("uid", headers.get("x-uid"));
+		//haspMap.put("uid", headers.get("x-uid"));
 		haspMap.put("data", strData);
 		
 		try {
-			Socket socket = new Socket("192.168.1.130", 2153);
+			Socket socket = new Socket("172.20.10.3", 2153);
 			JsonObject calSignTask = new JsonObject();
 			calSignTask.addProperty("id", 1000);
 			calSignTask.addProperty("action", "CalsignAction");
@@ -150,11 +248,8 @@ public class PyMain {
 			String str = new String(buffer, 0, n);
 			return new JsonParser().parse(str).getAsJsonObject().get("ret_msg").getAsString();
 		} catch (Exception e) {
-
 			e.printStackTrace();
 			return null;
 		}
-
-		
 	}
 }
--- CrawlService/src/main/java/com/tencent/mm/PyMain.java
+++ CrawlService/src/main/java/com/tencent/mm/PyMain.java
@@ -2,6 +2,9 @@ package com.tencent.mm;
 
 import java.io.IOException;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -11,56 +14,120 @@ import okhttp3.Response;
 
 public class PyMain {
 
-	public static void main(String[] args) throws IOException {
-		String url =// "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=AzSLtbbTvGn1Uypr2M5qc10%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN";
+	public static void test() {
+		String url = // "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=AzSLtbbTvGn1Uypr2M5qc10%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN";
 				"https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=A75XMX4lypn62t0w8OA%2Fn8A%3D&pass_ticket=%2Bl1vAECe7aI1PwFfIuz0Fl5uYt3JsBtPtRTb7HJ1gb0ttYCZ%2F1DSs9QOfeTQ22wr&wechat_real_lang=zh_CN";
-		
-		//String cache-control: max-age=0
-		//origin: https://szsupport.weixin.qq.com
-		//upgrade-insecure-requests: 1
-		//content-type: application/x-www-form-urlencoded
-		//user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 6P Build/MDA89D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044606 Mobile Safari/537.36 MMWEBID/7880 MicroMessenger/7.0.3.1400(0x27000334) Process/tools NetType/WIFI Language/zh_CN
-		//accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,image/wxpic,image/sharpp,image/apng,image/tpg,*/*;q=0.8
-		String referer= "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=AzSLtbbTvGn1Uypr2M5qc10%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN";
-		//accept-encoding: gzip, deflate, br
-		//accept-language: zh-CN,en-US;q=0.9
-		//q-ua2: QV=3&PL=ADR&PR=WX&PP=com.tencent.mm&PPVN=7.0.3&TBSVC=43620&CO=BK&COVC=044606&PB=GE&VE=GA&DE=PHONE&CHID=0&LCID=9422&MO= Nexus6P &RL=1440*2392&OS=6.0&API=23
-		//q-guid: 699adce8ef9a1144b3c583c913b788cb
-		//q-auth: 31045b957cf33acf31e40be2f3e71c5217597676a9729f1b
 
-		//forBlackberry=forceToUsePost
-		
-		
-		//method: POST
-		//authority: szsupport.weixin.qq.com
-		
+		// String cache-control: max-age=0
+		// origin: https://szsupport.weixin.qq.com
+		// upgrade-insecure-requests: 1
+		// content-type: application/x-www-form-urlencoded
+		// user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 6P Build/MDA89D;
+		// wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0
+		// Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044606 Mobile Safari/537.36
+		// MMWEBID/7880 MicroMessenger/7.0.3.1400(0x27000334) Process/tools
+		// NetType/WIFI Language/zh_CN
+		// accept:
+		// text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,image/wxpic,image/sharpp,image/apng,image/tpg,*/*;q=0.8
+		String referer = "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=AzSLtbbTvGn1Uypr2M5qc10%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN";
+		// accept-encoding: gzip, deflate, br
+		// accept-language: zh-CN,en-US;q=0.9
+		// q-ua2:
+		// QV=3&PL=ADR&PR=WX&PP=com.tencent.mm&PPVN=7.0.3&TBSVC=43620&CO=BK&COVC=044606&PB=GE&VE=GA&DE=PHONE&CHID=0&LCID=9422&MO=
+		// Nexus6P &RL=1440*2392&OS=6.0&API=23
+		// q-guid: 699adce8ef9a1144b3c583c913b788cb
+		// q-auth: 31045b957cf33acf31e40be2f3e71c5217597676a9729f1b
 
-		//forBlackberry=forceToUsePost
-		
-		try{
+		// forBlackberry=forceToUsePost
+
+		// method: POST
+		// authority: szsupport.weixin.qq.com
+
+		// forBlackberry=forceToUsePost
+
+		try {
 			OkHttpClient okHttpClient = new OkHttpClient();
-			final Request request = new Request.Builder().url("https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=A75XMX4lypn62t0w8OA%2Fn8A%3D&pass_ticket=%2Bl1vAECe7aI1PwFfIuz0Fl5uYt3JsBtPtRTb7HJ1gb0ttYCZ%2F1DSs9QOfeTQ22wr&wechat_real_lang=zh_CN")
-//					.header("referer", referer)
-//					.header("cache-control", "max-age=0")
-//					.header("origin", "https://szsupport.weixin.qq.com")
-//					.header("upgrade-insecure-requests", "1")
-//					.header("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 6P Build/MDA89D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044606 Mobile Safari/537.36 MMWEBID/7880 MicroMessenger/7.0.3.1400(0x27000334) Process/tools NetType/WIFI Language/zh_CN")
-//					.header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,image/wxpic,image/sharpp,image/apng,image/tpg,*/*;q=0.8")
-//					.header("referer", "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=Awu3hjXSuX4IxHj8Xh7odEE%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN")
-//					.header("accept-encoding", "gzip, deflate, br")
-//					.header("accept-language", "zh-CN,en-US;q=0.9")
-//					.header("q-ua2", "QV=3&PL=ADR&PR=WX&PP=com.tencent.mm&PPVN=7.0.3&TBSVC=43620&CO=BK&COVC=044606&PB=GE&VE=GA&DE=PHONE&CHID=0&LCID=9422&MO= Nexus6P &RL=1440*2392&OS=6.0&API=23")
-//					.header("q-guid", "699adce8ef9a1144b3c583c913b788cb")
-//					.header("q-auth", "31045b957cf33acf31e40be2f3e71c5217597676a9729f1b")
-					//.post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), "forBlackberry=forceToUsePost"))
+			final Request request = new Request.Builder().url(
+					"https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=A75XMX4lypn62t0w8OA%2Fn8A%3D&pass_ticket=%2Bl1vAECe7aI1PwFfIuz0Fl5uYt3JsBtPtRTb7HJ1gb0ttYCZ%2F1DSs9QOfeTQ22wr&wechat_real_lang=zh_CN")
+					// .header("referer", referer)
+					// .header("cache-control", "max-age=0")
+					// .header("origin", "https://szsupport.weixin.qq.com")
+					// .header("upgrade-insecure-requests", "1")
+					// .header("user-agent", "Mozilla/5.0 (Linux; Android 6.0;
+					// Nexus 6P Build/MDA89D; wv) AppleWebKit/537.36 (KHTML,
+					// like Gecko) Version/4.0 Chrome/66.0.3359.126
+					// MQQBrowser/6.2 TBS/044606 Mobile Safari/537.36
+					// MMWEBID/7880 MicroMessenger/7.0.3.1400(0x27000334)
+					// Process/tools NetType/WIFI Language/zh_CN")
+					// .header("accept",
+					// "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,image/wxpic,image/sharpp,image/apng,image/tpg,*/*;q=0.8")
+					// .header("referer",
+					// "https://szsupport.weixin.qq.com/cgi-bin/mmsupport-bin/addchatroombyqrcode?uuid=AVoaVXL_wsTwyVnS&exportkey=Awu3hjXSuX4IxHj8Xh7odEE%3D&pass_ticket=TtXeGtcczN%2BvFmnil6EeNeIJVKsuk0BA%2B3Vu0gZOtrc4LSWR2bVqgw3UyjZ7vMZK&wechat_real_lang=zh_CN")
+					// .header("accept-encoding", "gzip, deflate, br")
+					// .header("accept-language", "zh-CN,en-US;q=0.9")
+					// .header("q-ua2",
+					// "QV=3&PL=ADR&PR=WX&PP=com.tencent.mm&PPVN=7.0.3&TBSVC=43620&CO=BK&COVC=044606&PB=GE&VE=GA&DE=PHONE&CHID=0&LCID=9422&MO=
+					// Nexus6P &RL=1440*2392&OS=6.0&API=23")
+					// .header("q-guid", "699adce8ef9a1144b3c583c913b788cb")
+					// .header("q-auth",
+					// "31045b957cf33acf31e40be2f3e71c5217597676a9729f1b")
+					// .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+					// "forBlackberry=forceToUsePost"))
 					.build();
 			Call call = okHttpClient.newCall(request);
 			Response response = call.execute();
 			System.out.println("ssss:" + response.header("location"));
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
 
+	
+	public static final MediaType JSON = MediaType.parse("application/json");
+	public static String  login (String userid, String password) throws Exception{
+		JsonObject json = new JsonObject();
+
+		// json.
+		json.addProperty("user_id", userid);
+		json.addProperty("password", password);
+
+		Request req = new Request.Builder().url("https://andromeda.dev:8443/api_rad/v1/auth")
+				.post(RequestBody.create(JSON, json.toString())).build();
+		OkHttpClient okHttpClient = new OkHttpClient();
+		Response resp = okHttpClient.newCall(req).execute();
+	//	System.out.println("" + resp.body().string());
+		JsonObject res = new JsonParser().parse(resp.body().string()).getAsJsonObject();
+		return res.get("payload").getAsJsonObject().get("token").getAsString();
+
+	}
+
+	public static String getRepyMessage(String token ) throws Exception{
+		
+		JsonObject json = new JsonObject();
+		json.addProperty("timestamp", "1563345642");
+		json.addProperty("receiver_id" , "14041950753@chatroom");
+		json.addProperty("sender_id", "friendid");
+		json.addProperty("message", "fruit");
+		json.addProperty("attachments", "[]");
+		
+		Request req = new Request.Builder().url("http://andromeda.dev:15007/wechat/receive/group")
+				.post(RequestBody.create(JSON, json.toString()))
+				.header("Authorization", "Bearer "+ token)
+				.build();
+		
+		OkHttpClient okHttpClient = new OkHttpClient();
+		Response resp = okHttpClient.newCall(req).execute();
+		System.out.println("" + resp.body().string());
+		return null;
+		
+	}
+	public static void main(String[] args) {
+		try {
+			String token = login("aefa03e9-cce9-4efd-ad9d-132bec23d19c", "test");
+			getRepyMessage(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
